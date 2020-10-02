ed44c20eaf74899dad005e54ad706ed236f6e8b3
venkatasuryanarayana <32256493+venkatasuryanarayana@users.noreply.github.com>
2020-10-02 15:56:14 +0530

    added

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Surya from './surya.js';
+
+function App() {
+  return (
+    <div className="App">
+     <Surya/>
+    </div>
+  );
+}
+
+export default App;
